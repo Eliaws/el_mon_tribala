fb6d6e3fcb74137d8636fc65c5954d84cad5a5c6
Simon Saye Babu <114667196+SimonSayeBabu@users.noreply.github.com>
2026-05-18 20:07:12 +0200

    ✨ feat(View): Defined methods

--- src/view/View.java
+++ src/view/View.java
@@ -1,5 +1,21 @@
 package view;
 
-public class View {
-    
+import model.GameState;
+
+public interface View {
+
+    public void renderPlaying(GameState gameState);
+
+    public void renderShop(GameState gameState);
+
+    public void renderGameOver(GameState gameState);
+
+    public void renderVictory(GameState gameState);
+
+    public String getUserInput();
+
+    public void renderInvalidInput();
+
+    public void renderHelp();
+
 }
